791b65f843ce913a9c960c0bc46321c4c984bb67
Ronum96 <77383555+Ronum96@users.noreply.github.com>
2021-02-05 10:12:27 +0500

    HW7

---
+++ Plate.java
@@ -0,0 +1,13 @@
+package Models;
+
+public class Plate {
+    private int foodCount;
+
+    public  int getFoodCount() {
+        return foodCount;
+    }
+
+    public void setFoodCount(int foodCount) {
+        this.foodCount = foodCount;
+    }
+}
